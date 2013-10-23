ed6f22123ec45d5c0c68f3a176a8b0423a1e2976
Lucas Silvestre <lukas.silvestre@gmail.com>
2013-10-23 23:00:23 +0100

    Apply twetter bootstrap

--- instagram/index.php
+++ instagram/index.php
@@ -1,65 +1,141 @@
+<?php
+	// Supply a user id and an access token
+	$instagram['userid'] = "614631";
+	$instagram['accessToken'] = "614631.ab103e5.86703194cbee4b3cbf7efd02920d8a71";
+	$foursquare['oauth_token'] = "Y20FGZX51DY2LU5NIJE2FLTFS1BASWAVZEF25GQWT1F2MBHN";
+	$foursquare['query'] = "mineirao";
+
+	// Gets our data
+	function fetchData($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+		$result = curl_exec($ch);
+		curl_close($ch); 
+		return $result;
+	}
+
+	if($_GET['location']) {
+		$foursquare_v2_id = $_GET['location'];
+		$location_result = fetchData("https://api.instagram.com/v1/locations/search?foursquare_v2_id={$foursquare_v2_id}&access_token={$instagram['accessToken']}");
+		$location_result = json_decode($location_result);
+
+		foreach ($location_result->data as $location) {
+			$locationid = $location->id;	
+		}
+
+		// Pulls and parses data.
+		$result = fetchData("https://api.instagram.com/v1/locations/{$locationid}/media/recent?access_token={$instagram['accessToken']}");
+		$result = json_decode($result);
+	} else {
+		$foursquare_result = fetchData("https://api.foursquare.com/v2/venues/search?intent=global&query={$foursquare['query']}&oauth_token={$foursquare['oauth_token']}&v=20131022");
+		$foursquare_result = json_decode($foursquare_result);
+
+		foreach ($foursquare_result->response->venues as $location) {
+			// $foursquare_v2_id = $location->id;	
+			echo "<a href='/instagram/?location={$location->id}'>{$location->name}</a><br/>";
+		}
+	}
+?>
+
+
 <!DOCTYPE html>
-<html>
-	<link rel="stylesheet" href="jquery.fancybox-1.3.4.css" type="text/css">
-	<script type='text/javascript' src='jquery.min.js'></script>
-	<script type='text/javascript' src='jquery.fancybox-1.3.4.pack.js'></script>
-	<script type="text/javascript">
-		$(function() {
-			$("a.group").fancybox({
-				'nextEffect'	:	'fade',
-				'prevEffect'	:	'fade',
-				'overlayOpacity' :  0.8,
-				'overlayColor' : '#000000',
-				'arrows' : false,
-			});			
-		});
-	</script>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Lucas Silvestre">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
 
-	<h1>Cloud Project</h1>
+    <title>Project for Cloud Computing class</title>
 
-	<?php
-		// Supply a user id and an access token
-		$instagram['userid'] = "614631";
-		$instagram['accessToken'] = "614631.ab103e5.86703194cbee4b3cbf7efd02920d8a71";
-		$foursquare['oauth_token'] = "Y20FGZX51DY2LU5NIJE2FLTFS1BASWAVZEF25GQWT1F2MBHN";
-		$foursquare['query'] = "mineirao";
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/jquery.autocomplete.css">
 
-		// Gets our data
-		function fetchData($url) {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-			$result = curl_exec($ch);
-			curl_close($ch); 
-			return $result;
-		}
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="assets/css/jumbotron-narrow.css">
 
-		if($_GET['location']) {
-			$foursquare_v2_id = $_GET['location'];
-			$location_result = fetchData("https://api.instagram.com/v1/locations/search?foursquare_v2_id={$foursquare_v2_id}&access_token={$instagram['accessToken']}");
-			$location_result = json_decode($location_result);
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
 
-			foreach ($location_result->data as $location) {
-				$locationid = $location->id;	
-			}
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    
+    <script type="text/javascript" src="assets/js/jquery.autocomplete.js"></script>
 
-			// Pulls and parses data.
-			$result = fetchData("https://api.instagram.com/v1/locations/{$locationid}/media/recent?access_token={$instagram['accessToken']}");
-			$result = json_decode($result);
-		} else {
-			$foursquare_result = fetchData("https://api.foursquare.com/v2/venues/search?intent=global&query={$foursquare['query']}&oauth_token={$foursquare['oauth_token']}&v=20131022");
-			$foursquare_result = json_decode($foursquare_result);
+  </head>
 
-			foreach ($foursquare_result->response->venues as $location) {
-				// $foursquare_v2_id = $location->id;	
-				echo "<a href='/instagram/?location={$location->id}'>{$location->name}</a><br/>";
-			}
-		}
-	?>
+  <body>
 
-	<?php foreach ($result->data as $post): ?>
-		<!-- Renders images. @Options (thumbnail,low_resoulution, high_resolution) -->
-		<a class="group" rel="group1" href="<?= $post->images->standard_resolution->url ?>"><img src="<?= $post->images->thumbnail->url ?>"></a>
-	<?php endforeach ?>
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <h3 class="text-muted">Pic by Loc</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Choose your venue</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <div class="input-group input-group-lg">
+		      <input type="text" class="form-control" id="input_venue" name="input_venue" value="" />
+		      <input type="hidden" class="form-control" id="venue_id" name="venue_id" value="" />
+		      <span class="input-group-btn">
+		        <button class="btn btn-default" type="button">Go!</button>
+		      </span>
+		    </div><!-- /input-group -->
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+      <div class="footer">
+        <p>&copy; Company 2013</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <?php foreach ($result_asdf->data as $post): ?>
+			<!-- Renders images. @Options (thumbnail,low_resoulution, high_resolution) -->
+			<a class="group" rel="group1" href="<?= $post->images->standard_resolution->url ?>"><img src="<?= $post->images->thumbnail->url ?>"></a>
+		<?php endforeach ?>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    
+  </body>
 </html>
